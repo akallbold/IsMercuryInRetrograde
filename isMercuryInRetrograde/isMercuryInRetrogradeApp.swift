1b4ecbab4be799e86a904d1cbf273e3b48d78582
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2023-04-15 16:38:01 -0700

    Initial Commit

---
+++ isMercuryInRetrograde/isMercuryInRetrogradeApp.swift
@@ -0,0 +1,17 @@
+//
+//  isMercuryInRetrogradeApp.swift
+//  isMercuryInRetrograde
+//
+//  Created by Anna on 4/15/23.
+//
+
+import SwiftUI
+
+@main
+struct isMercuryInRetrogradeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
